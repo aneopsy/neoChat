57637fe314168de54c2918a10c3ae258dba95907
AneoPsy <paultevatheis@gmail.com>
2016-09-26 16:40:35 -1000

    modifié :         main.py

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ from src.userpref import UserPref
 from PyQt4 import QtCore, QtGui
 import sys
 
+if __name__ == "__main__":
+    app = QtGui.QApplication(sys.argv)
+    palette = QtGui.QPalette()
+    palette.setColor(QtGui.QPalette.Background, QtGui.QColor(33, 37, 43))
+    app.setPalette(palette)
+    sshFile = "resources/darkorange.stylesheet"
+    with open(sshFile, "r") as fh:
+        app.setStyleSheet(fh.read())
+    usernameWindow = UserPref()
+    usernameWindow.show()
 
-app = QtGui.QApplication(sys.argv)
-palette = QtGui.QPalette()
-palette.setColor(QtGui.QPalette.Background, QtGui.QColor(33, 37, 43))
-app.setPalette(palette)
-sshFile = "resources/darkorange.stylesheet"
-with open(sshFile, "r") as fh:
-    app.setStyleSheet(fh.read())
-usernameWindow = UserPref()
-usernameWindow.show()
-
-sys.exit(app.exec_())
+    sys.exit(app.exec_())
